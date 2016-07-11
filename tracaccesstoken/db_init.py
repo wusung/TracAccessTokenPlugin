db3a7f5ccd6322d7c3bb85080539a3a449f10937
Wusung Peng <wusungpeng@kkbox.com>
2016-07-11 08:22:17 +0000

    feat: add init scripts for new tables

---
+++ tracaccesstoken/db_init.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import argparse
+
+from trac.db import Table, Column, Index, DatabaseManager
+from trac.env import Environment
+
+schema = [
+    Table('kkbox_trac_api_key', key='id')[
+        Column('id', auto_increment=True),
+        Column('key'),
+        Column('username'),
+        Column('change_time', type='int64'),
+        Column('create_time', type='int64')
+    ]
+]
+
+parser = argparse.ArgumentParser()
+parser.add_argument("path", help="The root path of Trac environment")
+parser.add_argument("-c", "--create-tables", action="store_true",
+                    help = "Create tables",)
+parser.add_argument("-d", "--drop-tables", action="store_true",
+                    help="Drop tables",)
+args = parser.parse_args()
+
+env = Environment(args.path)
+databaseManager = DatabaseManager(env)
+
+
+def create_tables():
+    databaseManager.create_tables(schema)
+
+
+def drop_tables():
+    databaseManager.drop_tables(schema)
+
+
+if __name__ == '__main__':
+    if args.create_tables:
+        create_tables()
+    elif args.drop_tables:
+        drop_tables()
+    else:
+        pass
